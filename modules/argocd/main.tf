a58a46ed1fae2cdef30daff3d72a4a9cc827b1a5
Vinicius Alves David Da Silva <P02013@unimednacional.coop.br>
2026-04-26 00:36:51 -0300

    fix: argocd module usa bash em vez de powershell

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -29,29 +29,3 @@ resource "helm_release" "argocd" {
 
   depends_on = [kubernetes_namespace_v1.argocd]
 }
-
-resource "null_resource" "ingress_nginx" {
-  provisioner "local-exec" {
-    command = "kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.9.6/deploy/static/provider/cloud/deploy.yaml"
-  }
-  depends_on = [helm_release.argocd]
-}
-
-resource "null_resource" "sealed_secrets" {
-  provisioner "local-exec" {
-    command = "helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets && helm repo update && helm upgrade --install sealed-secrets sealed-secrets/sealed-secrets -n sealed-secrets --create-namespace --wait"
-  }
-  depends_on = [helm_release.argocd]
-}
-
-resource "null_resource" "argocd_apps" {
-  triggers = {
-    helm_release_id = helm_release.argocd.id
-  }
-
-  provisioner "local-exec" {
-    command = "kubectl apply --server-side --force-conflicts -k ${var.cd_apps_path}"
-  }
-
-  depends_on = [helm_release.argocd]
-}
