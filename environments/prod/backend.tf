c39a33fb624cd2d8098c6f3fde60e48dd76bd83d
Vinicius Alves David Da Silva <P02013@unimednacional.coop.br>
2026-05-05 17:03:48 -0300

    chore: migrate tfstate bucket to new AWS account

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "techchallenge4-togglemaster-state"
+    bucket = "techchallenge4-togglemaster-state-v2"
     key    = "techchallenge4/prod/terraform.tfstate"
     region = "us-east-1"
   }
